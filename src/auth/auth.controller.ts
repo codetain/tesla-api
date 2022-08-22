12481a5df7f912a5b7540553fd3d1879e22db46d
Michal-Turbanski <63620671+Michal-Turbanski@users.noreply.github.com>
2022-08-22 12:53:37 +0200

    Inject AuthService and create empty login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService: AuthService){}
+
+    @Get('/login')
+    login(){
+        return this.authService.login();
+    }
+}
