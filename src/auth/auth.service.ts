3b8ef37e2f7afc7c30fe534dd308cb2162b91ff9
Michal-Turbanski <63620671+Michal-Turbanski@users.noreply.github.com>
2022-08-25 11:58:52 +0200

    make first GET request

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,36 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { URL } from 'url';
+const crypto = require('crypto');
 
 @Injectable()
 export class AuthService {
 
     async login(){
-        // const response = await fetch('http://api.nbp.pl/api/cenyzlota');
-        // if(response.ok){
-        //     const data = await response.json();
-        //     console.log(data);
-        // }
-
         return this.firstGETrequest();
     }
 
-    firstGETrequest(){
+    async firstGETrequest(): Promise<string | null>{
         const firstURL = new URL(`https://auth.tesla.com/oauth2/v3/authorize`);
 
         const code_verifier:string = this.makeCodeVerifier(86);
 
         const client_id = 'ownerapi';
-        const code_challenge = '123';
+        const code_challenge = crypto.createHash('sha256').update(code_verifier).digest('base64');
+        const code_challenge_method: string = 'S256';
+        const redirect_uri = 'https://auth.tesla.com/void/callback';
+        const response_type = 'code';
+        const scope = 'openid email offline_access';
+        const state = '123';
 
         firstURL.searchParams.append('client_id', client_id);
         firstURL.searchParams.append('code_challenge', code_challenge);
-        firstURL.searchParams.append('code_challenge_method', 'S256');
-        firstURL.searchParams.append('redirect_uri', 'https://auth.tesla.com/void/callback');
-        firstURL.searchParams.append('response_type', 'code');
-        firstURL.searchParams.append('scope', 'openid email offline_access');
-        firstURL.searchParams.append('state', '123');
+        firstURL.searchParams.append('code_challenge_method', code_challenge_method);
+        firstURL.searchParams.append('redirect_uri', redirect_uri);
+        firstURL.searchParams.append('response_type', response_type);
+        firstURL.searchParams.append('scope', scope);
+        firstURL.searchParams.append('state', state);
 
-        return firstURL;
+        const res = await fetch(firstURL);
+        if(res.ok){
+            const htmlPage = await res.text();
+            return htmlPage;
+        }
+        return null;
     }
 
     makeCodeVerifier(length: number): string {
